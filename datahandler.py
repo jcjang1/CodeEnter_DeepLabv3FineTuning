fbb99c34ca6d6e3a8d9c79ccad7492cca83c353e
jcjang1 <wndptn1001@naver.com>
2022-07-24 11:56:13 +0000

    Binary

--- datahandler.py
+++ datahandler.py
@@ -15,6 +15,56 @@ from torchvision import transforms
 from segdataset import SegmentationDataset
 
 
+def get_dataloader_sep_folder(data_dir: str,
+                              image_folder: str = 'Images',
+                              mask_folder: str = 'Masks',
+                              batch_size: int = 4):
+    """ Create Train and Test dataloaders from two
+        separate Train and Test folders.
+        The directory structure should be as follows.
+        data_dir
+        --Train
+        ------Images
+        ---------Image1
+        ---------ImageN
+        ------Masks
+        ---------Mask1
+        ---------MaskN
+        --Test
+        ------Images
+        ---------Image1
+        ---------ImageM
+        ------Masks
+        ---------Mask1
+        ---------MaskM
+    Args:
+        data_dir (str): The data directory or root.
+        image_folder (str, optional): Image folder name. Defaults to 'Image'.
+        mask_folder (str, optional): Mask folder name. Defaults to 'Mask'.
+        batch_size (int, optional): Batch size of the dataloader. Defaults to 4.
+    Returns:
+        dataloaders: Returns dataloaders dictionary containing the
+        Train and Test dataloaders.
+    """
+    data_transforms = transforms.Compose([transforms.ToTensor()])
+
+    image_datasets = {
+        x: SegmentationDataset(root=Path(data_dir) / x,
+                               transforms=data_transforms,
+                               image_folder=image_folder,
+                               mask_folder=mask_folder)
+        for x in ['Train', 'Test']
+    }
+    dataloaders = {
+        x: DataLoader(image_datasets[x],
+                      batch_size=batch_size,
+                      shuffle=True,
+                      num_workers=8)
+        for x in ['Train', 'Test']
+    }
+    return dataloaders
+
+
 def get_dataloader_single_folder(data_dir: str,
                                  image_folder: str = 'Images',
                                  mask_folder: str = 'Masks',
@@ -22,14 +72,12 @@ def get_dataloader_single_folder(data_dir: str,
                                  batch_size: int = 4):
     """Create train and test dataloader from a single directory containing
     the image and mask folders.
-
     Args:
         data_dir (str): Data directory path or root
         image_folder (str, optional): Image folder name. Defaults to 'Images'.
         mask_folder (str, optional): Mask folder name. Defaults to 'Masks'.
         fraction (float, optional): Fraction of Test set. Defaults to 0.2.
         batch_size (int, optional): Dataloader batch size. Defaults to 4.
-
     Returns:
         dataloaders: Returns dataloaders dictionary containing the
         Train and Test dataloaders.
@@ -50,8 +98,7 @@ def get_dataloader_single_folder(data_dir: str,
         x: DataLoader(image_datasets[x],
                       batch_size=batch_size,
                       shuffle=True,
-                      num_workers=8,
-                      drop_last=True)
+                      num_workers=8)
         for x in ['Train', 'Test']
     }
     return dataloaders
